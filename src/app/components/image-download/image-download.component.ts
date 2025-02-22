dc49860fe83da7824e8b6bf428bcb0f87a59ad96
Lars Abelsson <georg483@msn.com>
2025-02-22 13:26:06 +0100

    Added empty download component

---
+++ src/app/components/image-download/image-download.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-download',
+  imports: [],
+  templateUrl: './image-download.component.html',
+  styleUrl: './image-download.component.scss'
+})
+export class ImageDownloadComponent {
+
+}
--- src/app/pages/image-auto-tag/image-auto-tag.component.ts
+++ src/app/pages/image-auto-tag/image-auto-tag.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { ImageUploadComponent } from '../../components/image-upload/image-upload.component';
 import { ImageViewComponent } from '../../components/image-view/image-view.component';
 import { MetadataEditorComponent } from '../../components/metadata-editor/metadata-editor.component';
+import { ImageDownloadComponent } from '../../components/image-download/image-download.component';
 
 @Component({
   selector: 'app-image-auto-tag',
-  imports: [ImageUploadComponent, ImageViewComponent, MetadataEditorComponent],
+  imports: [
+    ImageUploadComponent,
+    ImageViewComponent,
+    MetadataEditorComponent,
+    ImageDownloadComponent,
+  ],
   templateUrl: './image-auto-tag.component.html',
   styleUrl: './image-auto-tag.component.scss',
 })
